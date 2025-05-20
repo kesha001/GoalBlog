510debd7ec799c50103cf0c26698c5084ca60bea
Danylo Berdnyk <berdnykdanylo001@gmail.com>
2025-05-20 14:24:17 +0300

    base html page, dummy data

--- app/routes.py
+++ app/routes.py
@@ -9,6 +9,20 @@ def index():
 
     user = {'username': 'John'}
     # list of goals , think about structure of these goals
+    # it should be a group of goals with title and goals inside
+    goals = [
+    
+    {
+        "author": {'username': 'John'},
+        "title": "Make a goal",
+        "body": "make that goal is on webpage"
+    },
+    {
+        "author": {'username': 'Susan'},
+        "title": "Find a job",
+        "body": "learn, get skills, pass intervju"
+    },
+    ]
 
 
-    return render_template('index.html')
+    return render_template('index.html', user=user, goals=goals)
